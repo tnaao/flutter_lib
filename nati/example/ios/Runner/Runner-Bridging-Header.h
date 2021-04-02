7bdeccee3acc358275c6eeb85bcbd66203097dc3
sevenskid <sevenskid00@Gmail.com>
2021-04-02 16:20:22 +0800

    up ios

--- nati/example/ios/Runner/Runner-Bridging-Header.h
+++ nati/example/ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,6 @@
 #import "GeneratedPluginRegistrant.h"
+//导入UMCommon的OC的头文件
+#import <UMCommon/UMCommon.h>
+
+//导入UMVerify的OC的头文件
+#import <UMVerify/UMVerify.h>
--- nati/ios/Classes/SwiftNatiPlugin.swift
+++ nati/ios/Classes/SwiftNatiPlugin.swift
@@ -2,13 +2,19 @@ import Flutter
 import UIKit
 
 public class SwiftNatiPlugin: NSObject, FlutterPlugin {
+    static let CHANNEL_NAME = "com.leaf.fli/nati"
+    
+    public static var viewController = UIViewController()
+    var pendingResult:FlutterResult!
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "nati", binaryMessenger: registrar.messenger())
+    viewController = (UIApplication.shared.delegate?.window??.rootViewController)!
+    let channel = FlutterMethodChannel(name: CHANNEL_NAME, binaryMessenger: registrar.messenger())
     let instance = SwiftNatiPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    pendingResult = result
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
