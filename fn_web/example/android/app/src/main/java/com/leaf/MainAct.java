f3aae01cfb950cce61dc7a268225115019bea2d4
mbp <sevenskid00@gmail.com>
2024-08-16 10:56:30 +0800

    init

---
+++ fn_web/example/android/app/src/main/java/com/leaf/MainAct.java
@@ -0,0 +1,14 @@
+package com.leaf;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainAct extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
